7deb7bf1e0a28040ad78e7b8677ee98ce2a2b4f4
mgaultiernz <mgaultiernz@gmail.com>
2021-04-08 09:43:46 +0200

    mise en place de l'envoi de mail

--- app/Http/Controllers/Auth/PasswordResetLinkController.php
+++ app/Http/Controllers/Auth/PasswordResetLinkController.php
@@ -32,12 +32,21 @@ class PasswordResetLinkController extends Controller
             'email' => 'required|email',
         ]);
 
+
+
+        $details = [
+            'title' => 'Mail from ItSolutionStuff.com',
+            'body' => 'This is for testing email using smtp'
+        ];
+
+
         // We will send the password reset link to this user. Once we have attempted
         // to send the link, we will examine the response then see the message we
         // need to show to the user. Finally, we'll send out a proper response.
         $status = Password::sendResetLink(
             $request->only('email')
         );
+
 
         return $status == Password::RESET_LINK_SENT
                     ? back()->with('status', __($status))
